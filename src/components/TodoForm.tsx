8e55dd30689d0c87b1ed0f634ca9389926c9cfe2
Hbeom00 <bum6968@naver.com>
2025-03-14 08:28:40 +0900

    데이터 조회 및 캐싱 처리 완료

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,7 +1,80 @@
 "use client";
 
+import { PostType } from "@/app/types/post";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
 const TodoForm = () => {
-  return <div>폼 컴포넌트 입니다.</div>;
+  const fetchTodos = async () => {
+    const res = await axios.get("http://localhost:3000/todos");
+    return res.data;
+  };
+
+  const {
+    data: todos,
+    isPending,
+    isError,
+  } = useQuery({
+    queryKey: ["todos"],
+    queryFn: fetchTodos,
+  });
+
+  if (isPending) {
+    return <div>로딩중입니다..</div>;
+  }
+
+  if (isError) {
+    return <div>조회 중 오류가 발생했습니다.</div>;
+  }
+
+  const todoList: PostType[] = todos.filter(
+    (el: PostType) => el.isDone === false
+  );
+
+  const doneList: PostType[] = todos.filter(
+    (el: PostType) => el.isDone === true
+  );
+
+  return (
+    <div>
+      <h1>Todos</h1>
+      <form>
+        <input type="text" />
+        <input type="text" />
+        <button>추가</button>
+      </form>
+
+      <h2>해야할 일</h2>
+      <ul>
+        {todoList.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <h3>{todo.title}</h3>
+              <p>{todo.content}</p>
+              <p>{todo.isDone ? "완료됨" : "미완료됨"}</p>
+              <button>삭제</button>
+              <button>완료</button>
+            </li>
+          );
+        })}
+      </ul>
+
+      <h2>완료된 일</h2>
+      <ul>
+        {doneList.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <h3>{todo.title}</h3>
+              <p>{todo.content}</p>
+              <p>{todo.isDone ? "완료됨" : "미완료됨"}</p>
+              <button>삭제</button>
+              <button>취소</button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default TodoForm;
